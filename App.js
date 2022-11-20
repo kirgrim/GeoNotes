d6c55ddc3df431c47e19b05ef8e82421dd38e5ec
kirill.grim <kirill.grim@gmail.com>
2022-11-20 20:49:53 +0100

    Added basic menu of application

--- App.js
+++ App.js
@@ -9,109 +9,53 @@
 import React from 'react';
 import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    Button,
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {HomeScreen} from "./stacks/Home";
+import {AddNote} from "./stacks/AddNote";
+import {DisplayNotes} from "./stacks/DisplayNotes";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const Stack = createStackNavigator();
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit123 <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    const ref = React.useRef(null);
+    return (
+        <View style={styles.container}>
+            <NavigationContainer ref={ref}>
+                <Stack.Navigator initialRouteName="Empty">
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="AddNote" component={AddNote} />
+                    <Stack.Screen name="DisplayNotes" component={DisplayNotes} />
+                </Stack.Navigator>
+            </NavigationContainer>
+            <Button
+                onPress={() => ref.current && ref.current.navigate('Home')}
+                title="My Profile"/>
+            <Button
+                onPress={() => ref.current && ref.current.navigate('DisplayNotes')}
+                title="My Notes"/>
+            <Button
+                onPress={() => ref.current && ref.current.navigate('AddNote')}
+                title="Add Note"/>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    padding: 50,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  item: {
+    padding: 20,
+    fontSize: 15,
+    marginTop: 5,
+  }
 });
 
 export default App;
